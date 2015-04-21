3238e2772cd7b860504127f0c340a3667e5d0206
Yaonxin <liufuyun88@gmail.com>
2015-04-22 07:55:26 +0800

    add lis in dp

--- algorithm/dp.cpp
+++ algorithm/dp.cpp
@@ -33,9 +33,34 @@ int MinCoin(int value)
 //一个序列有N个数：A[1],A[2],…,A[N]，求出最长非降子序列的长度 LIS：longest increasing subsequence
 //状态方程：d(i) = max{1, d(j) + 1} j < i 且A[j] <= A[i]
 
+//O(n)
 int lis(int A[], int n)
 {
-	
+	int *d  = new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		d[i] = 1;
+	}
+	int j = 0;
+	int len = 1;
+
+	for (int i = 0; i < n; ++i)
+	{
+		j = i - 1;
+		while(j > 0)
+		{
+			if(A[j] <= A[i] && d[j] + 1 > d[i])
+			{
+				d[i] = d[j] + 1;
+			}
+			j--;
+		}
+		if(len < d[i]) 
+			len = d[i];
+	}
+	delete []d;
+
+	return len;
 }
 
 
@@ -46,5 +71,8 @@ int main(int argc, char const *argv[])
 {
 	//test min coin
 	cout << MinCoin(11) << endl;
+
+	int a[] = {5, 3, 4, 8, 6, 7};
+	cout << lis(a, 6) << endl;
 	return 0;
 }
